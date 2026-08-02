7e0439e98886326f90f6b1c0b526ebb5c7c7d99c
real-Silent <idk7778001@gmail.com>
2026-08-02 21:23:07 +0100

    2.2.1

--- Arrakis/PluginInfo.cs
+++ Arrakis/PluginInfo.cs
@@ -5,7 +5,7 @@
         public const string GUID = "org.nova.arrakis";
         public const string Name = "Arrakis";
         public const string Description = "Created by nova (@novaissilly). and Sleepy";
-        public const string Version = "2.2.0";
+        public const string Version = "2.2.1";
         public const string BaseDirectory = "Arrakis";
         public const string ServerApi = "https://novax.lol/arrakis/api"; // Luckily still up
         public const string DiscordLink = "https://discord.gg/dtQdz59FJG ";
