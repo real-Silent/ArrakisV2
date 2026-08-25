9152cbe4a5262263cd7ea46c1ba04d4b10c8cb85
Strawberry2242 <kylumthornton@gmail.com>
2026-08-25 17:37:15 -0400

    change value

--- Arrakis/Mods/Overpowered.cs
+++ Arrakis/Mods/Overpowered.cs
@@ -508,7 +508,7 @@ namespace Arrakis.Mods
             if (PhotonNetwork.InRoom && Time.time >= LagDelay)
             {
                 LagDelay = Time.time + 9f;
-                for (int i = 0; i < 3000; i++)
+                for (int i = 0; i < 2000; i++)
                 {
                     PhotonNetwork.NetworkingClient.OpRaiseEvent(3, new object[0], new RaiseEventOptions
                     {
