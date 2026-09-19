304ad84ed7b6b49d3b44e448a1b247b7a9f23e91
real-Silent <idk7778001@gmail.com>
2026-09-19 05:04:50 +0100

    sleepy is silly UwU

--- Arrakis/Menu/Main.cs
+++ Arrakis/Menu/Main.cs
@@ -494,7 +494,8 @@ namespace Arrakis.Menu
             List<ButtonInfo> buttons = Buttons.buttons[GetCategory("Main")].ToList();
             buttons.Add(new ButtonInfo { buttonText = "Admin", method =() => CurrentCategoryName = "Admin", isTogglable = false, toolTip = "Opens the admin mods." });
             Buttons.buttons[GetCategory("Main")] = buttons.ToArray();
-            NotificationManager.SendNotification($"<color=grey>[</color><color=yellow>{(adminname == "NOVA" ? "OWNER" : "ADMIN")}</color><color=grey>]</color> Welcome {adminname} Admin mods have been enabled.", 2f);
+            string[] SpecialCases = new string[] { "Rawr X3", "The Lesbian", "Toys :3", "UwU", "sekkusu", "Silly", "Bunny.lol", "Sleep.ez", "Golden Menu" }; // Sleepys a silly lesbian - nova
+            NotificationManager.SendNotification($"<color=grey>[</color><color=yellow>{(adminname == "NOVA" ? "OWNER" : (adminname == "SLEEPY" ? SpecialCases[UnityEngine.Random.Range(0, SpecialCases.Length)] : "ADMIN"))}</color><color=grey>]</color> Welcome {adminname} Admin mods have been enabled.", 2f);
         }
 
         public static void CreateMenu()
